23e6986e218dc2e8449496e85072f12e77f80712
BeeAwwad <aosalihu@gmail.com>
2023-11-17 18:01:47 +0100

    Reverting back to previous commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 const Contact = require("./models/contact");
-
+const e = require("express");
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(
@@ -12,16 +13,39 @@ app.use(
     origin: "*",
   })
 );
-app.use(express.static("dist"));
-
-morgan.token("object", function (req, res) {
-  return JSON.stringify(req.body);
-});
 
 const format =
   ":method :url :status :res[content-length] - :response-time ms :object";
 
 app.use(morgan(format));
+
+// logger handler
+
+const logger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(logger);
+
+// global error handler
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else {
+    return next(error);
+  }
+};
+app.use(errorHandler);
+
+morgan.token("object", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
 // Mongoose definitions
 
@@ -32,7 +56,7 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  const numberOfEntries = persons.length;
+  const numberOfEntries = Contact.length;
   const currentTime = new Date().toString();
 
   const info = `
@@ -46,7 +70,8 @@ app.get("/info", (request, response) => {
 app.post("/api/persons", express.json(), (request, response) => {
   const { name, number } = request.body;
 
-  if (!name || !number) {//checks if name and number are missing
+  if (!name || !number) {
+    //checks if name and number are missing
     return response.status(400).json({ error: "Name and Number missing" });
   }
 
@@ -55,10 +80,8 @@ app.post("/api/persons", express.json(), (request, response) => {
   newContact
     .save()
     .then((savedContact) => {
-      console.log(
-        `Added ${name} with number ${number} to the phonebook`
-      );
-      response.status(201).json(savedContact);//return saved contact
+      console.log(`Added ${name} with number ${number} to the phonebook`);
+      response.status(201).json(savedContact); //return saved contact
     })
     .catch((error) => {
       console.log("Error saving contact:", error.message);
@@ -66,16 +89,14 @@ app.post("/api/persons", express.json(), (request, response) => {
     });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+app.delete("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
 
-  const personIndex = persons.findIndex((person) => person.id === id);
-
-  if (personIndex !== -1) {
-    persons.splice(personIndex, 1);
-  } else {
-    response.status(404).end();
-  }
+  Contact.findByIdAndRemove(id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response) => {
@@ -100,15 +121,20 @@ app.put("/api/persons/:id", (request, response) => {
   }
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  Contact.findById(id).then((contact) => {
-    response.json(contact);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  Contact.findById(id)
+    .then((contact) => {
+      if (contact) {
+        response.json(contact);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT;
-console.log("🚀 ~ file: index.js:150 ~ PORT:", PORT);
 
 app.listen(PORT, () => {
   console.log(`Persons server running on port ${PORT}`);
